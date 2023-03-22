eeecfd8a067477a3ecfd3adb61f843d032706b7b
merlimigor <122364169+merlimigor@users.noreply.github.com>
2023-03-22 11:17:53 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AddMovie from './AddMovie'
+
+function App() {
+   
+  return (
+    <div className="App">
+      <AddMovie/>
+      
+    </div>
+  );
+}
+
+export default App;
